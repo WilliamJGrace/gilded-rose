de604ff1cd64e30a6089d84821eac59b19129022
WilliamJGrace <williamjgrace@outlook.com>
2020-04-24 14:21:03 +0100

    G:Tests for aging brie

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,7 @@
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
 
-  describe("item", () =>{
+  describe("Item", () =>{
     it("should return an item name", function() {
       const item = new Item("foo", 1, 2)
       expect(item.name).toEqual("foo")
@@ -15,6 +15,43 @@ describe("Gilded Rose", function() {
       expect(item.quality).toEqual(2)
     });
 
+  })
+
+  describe("Shop", () => {
+    describe("#updateQuality", () => {
+      let item
+      let items
+      let shop
+      beforeEach(() => {
+        item = jasmine.createSpyObj('item',['name','sellIn','quality'] )
+      })
+      it("increases quality of brie by 1 as sell date decreases by 1", () => {
+        item.name = "Aged Brie"
+        item.sellIn = 3
+        item.quality = 0
+        items = [item]
+        shop = new Shop(items)
+
+        for(var i=0;i<3;i++){
+          shop.updateQuality()
+        }
+        expect(shop.items[0].quality).toEqual(3)
+      })
+      it("increases quality of  brie by 2 when sellin reahes 0", () => {
+        item.name = "Aged Brie"
+        item.sellIn = 3
+        item.quality = 0
+        items = [item]
+        shop = new Shop(items)
+
+        for(var i=0;i<5;i++){
+          shop.updateQuality()
+        }
+        expect(shop.items[0].quality).toEqual(7)
+
+
+      })
+    })
   })
 
 
