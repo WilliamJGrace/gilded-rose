cb1106c4e1155145c2ad6ff96615421861a38f98
WilliamJGrace <williamjgrace@outlook.com>
2020-04-24 14:45:34 +0100

    G: tests for normal items

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -25,6 +25,8 @@ describe("Gilded Rose", function() {
       beforeEach(() => {
         item = jasmine.createSpyObj('item',['name','sellIn','quality'] )
       })
+
+    //BRIE TESTS
       it("increases quality of brie by 1 as sell date decreases by 1", () => {
         item.name = "Aged Brie"
         item.sellIn = 3
@@ -48,9 +50,58 @@ describe("Gilded Rose", function() {
           shop.updateQuality()
         }
         expect(shop.items[0].quality).toEqual(7)
-
-
       })
+      it('The quality of item is never above 50', function() {
+        item.name = "Aged Brie"
+        item.sellIn = 5
+        item.quality = 48
+        items = [item]
+        shop = new Shop(items)
+        for(var i=0;i<6;i++){
+          shop.updateQuality()
+        }
+        expect(shop.items[0].quality).toEqual(50)
+
+
+      });
+      //TEST FOR NORMAL Item
+     it('descreses quality for a normal item by 1 per day', function() {
+       item.name = "Corn Flakes"
+       item.sellIn = 5
+       item.quality = 4
+       items = [item]
+       shop = new Shop(items)
+       for(var i=0;i<3;i++){
+         shop.updateQuality()
+       }
+       expect(shop.items[0].quality).toEqual(1)
+     });
+     it('decreases quality dounle as much when sell date reaches 0', function() {
+       item.name = "Corn Flakes"
+       item.sellIn = 5
+       item.quality = 10
+       items = [item]
+       shop = new Shop(items)
+       for(var i=0;i<6;i++){
+         shop.updateQuality()
+       }
+       expect(shop.items[0].quality).toEqual(3)
+     });
+     it('quality never goes below 0', function() {
+       item.name = "Corn Flakes"
+       item.sellIn = 5
+       item.quality = 3
+       items = [item]
+       shop = new Shop(items)
+       for(var i=0;i<6;i++){
+         shop.updateQuality()
+       }
+       expect(shop.items[0].quality).toEqual(0)
+
+     });
+
+
+
     })
   })
 
